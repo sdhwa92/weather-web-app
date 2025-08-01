b9283209876e4673bef218994cc32ac0a62ac8ac
Daehwa Seo <sdhwa92@gmail.com>
2025-08-01 23:38:04 +1000

    add index.css for global css styles

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { AdapterLuxon } from "@mui/x-date-pickers/AdapterLuxon";
 import App from "./App.jsx";
 import { AppProvider } from "./AppProvider.jsx";
 
+import "./index.css";
+
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
